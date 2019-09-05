5d0f69707dded87b1557b3e47a6eb7b48b72d889
Alexis Jouan <alexisjouan@vlan-2664-10-17-8-23.staff.wireless.sydney.edu.au>
2019-09-05 14:50:17 +1000

    add functions

--- JJ_measurements/R_T_yoko.py
+++ JJ_measurements/R_T_yoko.py
@@ -1,64 +1,65 @@
 # Measure the Resistance R as a function of temperature
 
-stanford_gain = 1
+def RT_yoko():
+    stanford_gain = 1
 
-dmm1.NPLC(10)
-#dmm2.NPLC(10)
+    dmm1.NPLC(10)
+    #dmm2.NPLC(10)
 
-yoko.current.step = 5e-9
-yoko.current.inter_delay = 10e-3
+    yoko.current.step = 5e-9
+    yoko.current.inter_delay = 10e-3
 
-meas = Measurement()
+    meas = Measurement()
 
-meas.register_parameter(yoko.current)
-meas.register_custom_parameter("Temperature")
-meas.register_custom_parameter("Counter")
-meas.register_custom_parameter("Resistance")
-meas.register_parameter(dmm1.volt, setpoints=("Temperature",))
-#meas.register_parameter(dmm2.volt, setpoints=("Temperature",))
+    meas.register_parameter(yoko.current)
+    meas.register_custom_parameter("Temperature")
+    meas.register_custom_parameter("Counter")
+    meas.register_custom_parameter("Resistance")
+    meas.register_parameter(dmm1.volt, setpoints=("Temperature",))
+    #meas.register_parameter(dmm2.volt, setpoints=("Temperature",))
 
-yoko.output('off')
-yoko.source_mode("CURR")
-yoko.output('on')
+    yoko.output('off')
+    yoko.source_mode("CURR")
+    yoko.output('on')
 
-j=0
+    j=0
 
-current = 1.0e-6
+    current = 1.0e-6
 
-T = ft()
+    T = ft()
 
-with meas.run() as datasaver:
-    
-    while T[1]>3:
+    with meas.run() as datasaver:
         
-        T = ft()
-        
-        yoko.current(current)
-        
-        time.sleep(1)
+        while T[1]>3:
+            
+            T = ft()
+            
+            yoko.current(current)
+            
+            time.sleep(1)
 
-        volt_p = dmm1.volt()/stanford_gain
-        
-        time.sleep(1)
-        
-        yoko.current(-current)
-        
-        time.sleep(1)
-        
-        volt_m = dmm1.volt()/stanford_gain
-        
-        V_av = (volt_p - volt_m)/2
-        
-        R_av = V_av/current
+            volt_p = dmm1.volt()/stanford_gain
+            
+            time.sleep(1)
+            
+            yoko.current(-current)
+            
+            time.sleep(1)
+            
+            volt_m = dmm1.volt()/stanford_gain
+            
+            V_av = (volt_p - volt_m)/2
+            
+            R_av = V_av/current
 
-        datasaver.add_result((yoko.current,current),
-                            ("Temperature",T),
-                            ("Counter",j),
-                             ("Resistance",R_av),
-                            (dmm1.volt,V_av))
-        
-        print((T[1],R_av))
-        time.sleep(300)
-        j = j+1
-        
-yoko.current(0)
+            datasaver.add_result((yoko.current,current),
+                                ("Temperature",T),
+                                ("Counter",j),
+                                 ("Resistance",R_av),
+                                (dmm1.volt,V_av))
+            
+            print((T[1],R_av))
+            time.sleep(300)
+            j = j+1
+            
+    yoko.current(0)
--- JJ_measurements/init.py
+++ JJ_measurements/init.py
@@ -45,6 +45,40 @@ def init():
 
 	scfg = StationConfigurator()
 
+	import requests
+	from qcodes import MultiParameter
+
+
+	class FridgeTemps(MultiParameter):
+	    def __init__(self, fridge, url):
+	        self.url = url
+	        
+	        params = requests.get(url)
+	        if params.status_code != 200:
+	            raise RuntimeError("Unable to query fridge")
+	        params = set(params.json().keys())
+	        params.remove("Time")
+	        params = tuple(params)
+	        self.params = params
+	        
+	        super().__init__(
+	                "{}_temps".format(fridge),
+	                names=params,
+	                shapes=tuple(() for _ in params),
+	                units=tuple("K" for _ in params),
+	                snapshot_get=False)
+	        
+	    def get_raw(self):
+	        temps = requests.get(self.url)
+	        if temps.status_code != 200:
+	            raise RuntimeError("Unable to query fridge")
+	        temps = temps.json()
+	        temps = [temps[therm] for therm in self.params]
+	        return tuple(temps)
+
+	ft = FridgeTemps("BlueFors_LD", 
+	     "https://qphys1114.research.ext.sydney.edu.au/therm_flask/BlueFors_LD/data/?current")
+
 	#lockin_1 = scfg.load_instrument('sr860_1')
 	#lockin_2 = scfg.load_instrument('sr860_2')
 	yoko = scfg.load_instrument('yoko')
