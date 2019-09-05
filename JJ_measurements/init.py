dec742330629183394584a8b11c6f45524202763
Alexis Jouan <alexisjouan@vlan-2664-10-17-8-23.staff.wireless.sydney.edu.au>
2019-09-05 14:45:35 +1000

    init function

--- JJ_measurements/init.py
+++ JJ_measurements/init.py
@@ -1,52 +1,54 @@
-import time
-import numpy as np
-import scipy
-import matplotlib.pyplot as plt
+def init():
 
-import MDAC
+	import time
+	import numpy as np
+	import scipy
+	import matplotlib.pyplot as plt
 
-import qcodes as qc
-from qcodes import ChannelList, Parameter
-from qcodes.dataset.measurements import Measurement
-from qcodes.dataset.plotting import plot_by_id
-from qcodes.dataset.data_set import load_by_id, load_by_counter
-from qcodes.dataset.experiment_container import new_experiment, load_experiment_by_name
+	import MDAC
 
-from qdev_wrappers.station_configurator import StationConfigurator
+	import qcodes as qc
+	from qcodes import ChannelList, Parameter
+	from qcodes.dataset.measurements import Measurement
+	from qcodes.dataset.plotting import plot_by_id
+	from qcodes.dataset.data_set import load_by_id, load_by_counter
+	from qcodes.dataset.experiment_container import new_experiment, load_experiment_by_name
 
-import qcodes_measurements as qcm
-from qcodes_measurements.plot.plot_tools import *
+	from qdev_wrappers.station_configurator import StationConfigurator
 
-# Log all output
-from IPython import get_ipython
-ip = get_ipython()
-ip.magic("logstop")
-ip.magic("logstart -o -t iPython_Logs\\console_log.py rotate")
+	import qcodes_measurements as qcm
+	from qcodes_measurements.plot.plot_tools import *
 
-# Close any instruments that may already be open
-instruments = list(qc.Instrument._all_instruments.keys())
-for instrument in instruments:
-    instr = qc.Instrument._all_instruments.pop(instrument)
-    instr = instr()
-    instr.close()
-del instruments
+	# Log all output
+	from IPython import get_ipython
+	ip = get_ipython()
+	ip.magic("logstop")
+	ip.magic("logstart -o -t iPython_Logs\\console_log.py rotate")
 
-exp_name = 'JJ_DC_meas'
-sample_name = 'Anod_JJ_B'
+	# Close any instruments that may already be open
+	instruments = list(qc.Instrument._all_instruments.keys())
+	for instrument in instruments:
+	    instr = qc.Instrument._all_instruments.pop(instrument)
+	    instr = instr()
+	    instr.close()
+	del instruments
 
-try:
-    exp = load_experiment_by_name(exp_name, sample=sample_name)
-    print('Experiment loaded. Last ID no:', exp.last_counter)
-except ValueError:
-    exp = new_experiment(exp_name, sample_name)
-    print('Starting new experiment.')
+	exp_name = 'JJ_DC_meas'
+	sample_name = 'Anod_JJ_B'
 
-scfg = StationConfigurator()
+	try:
+	    exp = load_experiment_by_name(exp_name, sample=sample_name)
+	    print('Experiment loaded. Last ID no:', exp.last_counter)
+	except ValueError:
+	    exp = new_experiment(exp_name, sample_name)
+	    print('Starting new experiment.')
 
-#lockin_1 = scfg.load_instrument('sr860_1')
-#lockin_2 = scfg.load_instrument('sr860_2')
-yoko = scfg.load_instrument('yoko')
-#mag = scfg.load_instrument('Oxford')
-dmm1 = scfg.load_instrument('agilent_1')
-dmm2 = scfg.load_instrument('agilent_2')
-#mdac = MDAC.MDAC('mdac_8', 'ASRL9::INSTR')
+	scfg = StationConfigurator()
+
+	#lockin_1 = scfg.load_instrument('sr860_1')
+	#lockin_2 = scfg.load_instrument('sr860_2')
+	yoko = scfg.load_instrument('yoko')
+	#mag = scfg.load_instrument('Oxford')
+	dmm1 = scfg.load_instrument('agilent_1')
+	dmm2 = scfg.load_instrument('agilent_2')
+	#mdac = MDAC.MDAC('mdac_8', 'ASRL9::INSTR')
