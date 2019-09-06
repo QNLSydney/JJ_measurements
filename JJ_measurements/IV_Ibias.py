e13512ae540f38ccdf5d12d2d337744f73d5c116
Alexis Jouan <alexis.jouan@sydney.edu.au>
2019-09-06 10:16:13 +1000

    First IV code working

--- JJ_measurements/IV_Ibias.py
+++ JJ_measurements/IV_Ibias.py
@@ -1,43 +1,43 @@
 # IV Ibias
+import numpy as np
 
-def IV_up():
-    
-	stanford_gain_1 = 1
+def IV_up(station, meas):
+
+	stanford_gain_1 = 1e3
 	stanford_gain_2 = 1e3
 
-	dmm1.volt()
-	dmm1.NPLC(1)
+	station.dmm1.volt()
+	station.dmm1.NPLC(1)
 
-	dmm2.volt()
-	dmm2.NPLC(1)
+	station.dmm2.volt()
+	station.dmm2.NPLC(1)
 
-	yoko.output('off')
-	yoko.source_mode("VOLT")
-	yoko.output('on')
+	station.yoko.output('off')
+	station.yoko.source_mode("VOLT")
+	station.yoko.output('on')
 
+	station.yoko.voltage.step = 5e-3
+	station.yoko.voltage.inter_delay = 10e-3
 
-	yoko.voltage.step = 5e-3
-	yoko.voltage.inter_delay = 10e-3
-
-	meas = Measurement()
-
-	meas.register_parameter(yoko.voltage)
-	meas.register_parameter(dmm2.volt)
-	meas.register_parameter(dmm1.volt, setpoints=(dmm2.volt,))
+	meas.register_parameter(station.yoko.voltage)
+	meas.register_parameter(station.dmm2.volt)
+	meas.register_custom_parameter("Current")
+	meas.register_parameter(station.dmm1.volt, setpoints=("Current",))
 
 
 	voltages =np.linspace(-50e-3,50e-3,101)
 
 	with meas.run() as datasaver:
 	    for v in voltages:
-	        yoko.voltage(v)
+	        station.yoko.voltage(v)
 
 	        
-	        voltage_meas = dmm1.volt()/stanford_gain_1
-	        current_meas = dmm2.volt()/(1e4*stanford_gain_2)
+	        voltage_meas = station.dmm1.volt()/stanford_gain_1
+	        current_meas = station.dmm2.volt()/(1e4*stanford_gain_2)
 
-	        datasaver.add_result((yoko.voltage,v),
-	                            (dmm2.volt,current_meas),
-	                            (dmm1.volt,voltage_meas))
+	        datasaver.add_result((station.yoko.voltage,v),
+	                            (station.dmm2.volt,station.dmm2.volt()),
+	                            ("Current",current_meas),
+	                            (station.dmm1.volt,voltage_meas))
 
-	yoko.voltage(0)
+	station.yoko.voltage(0)
