3be7b7dca8ff578a99e2fb674be2268a535518a9
Alexis Jouan <alexis.jouan@sydney.edu.au>
2019-09-10 16:55:34 +1000

    frige temp is back outside the R_T_yoko

--- JJ_measurements/R_T_yoko.py
+++ JJ_measurements/R_T_yoko.py
@@ -1,37 +1,7 @@
 # Measure the Resistance R as a function of temperature
 import time 
 
-class FridgeTemps(MultiParameter):
-    def __init__(self, fridge, url):
-        self.url = url
-        
-        params = requests.get(url)
-        if params.status_code != 200:
-            raise RuntimeError("Unable to query fridge")
-        params = set(params.json().keys())
-        params.remove("Time")
-        params = tuple(params)
-        self.params = params
-        
-        super().__init__(
-                "{}_temps".format(fridge),
-                names=params,
-                shapes=tuple(() for _ in params),
-                units=tuple("K" for _ in params),
-                snapshot_get=False)
-        
-    def get_raw(self):
-        temps = requests.get(self.url)
-        if temps.status_code != 200:
-            raise RuntimeError("Unable to query fridge")
-        temps = temps.json()
-        temps = [temps[therm] for therm in self.params]
-        return tuple(temps)
-
-ft = FridgeTemps("BlueFors_LD", 
-     "https://qphys1114.research.ext.sydney.edu.au/therm_flask/BlueFors_LD/data/?current")
-
-def RT_yoko(station, meas, voltage, stanford_gain_V, stanford_gain_I):
+def RT_yoko(station, meas, ft, voltage, stanford_gain_V, stanford_gain_I):
 
     station.dmm1.NPLC(10)
     station.dmm2.NPLC(10)
