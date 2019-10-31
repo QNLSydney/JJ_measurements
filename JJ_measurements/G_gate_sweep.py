449aa09fc1ae16f11f296438cb3a0ccd8ec679ab
Alexis Jouan <alexis.jouan@sydney.edu.au>
2019-10-31 11:06:24 +1100

    1D gate sweep lockin measurement

---
+++ JJ_measurements/G_gate_sweep.py
@@ -0,0 +1,10 @@
+#
+
+import numpy as np
+import time
+from qcodes.dataset.measurements import Measurement
+from qcodes.dataset.plotting import plot_by_id
+from datetime import datetime
+
+import qcodes_measurements as qcm
+from qcodes_measurements.tools.measure import _run_functions, _get_window
