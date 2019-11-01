67ff9b07c78baa72ef7bb106af988b3ccca9e291
Alexis Jouan <alexis.jouan@sydney.edu.au>
2019-11-01 11:00:09 +1100

    added gate function for Hall meas

--- JJ_measurements/Hall_meas.py
+++ JJ_measurements/Hall_meas.py
@@ -6,8 +6,9 @@ import numpy as np
 from qcodes.dataset.measurements import Measurement
 from qcodes.dataset.plotting import plot_by_id
 from datetime import datetime
+import time
 
-def Hall(V_polar, field_range_Y, stanford_gain_1, stanford_gain_2):
+def Hall(station, V_polar, field_range_Y, stanford_gain_1, stanford_gain_2, stanford_gain_3):
 
     R_polar = 1e6
 
@@ -31,6 +32,7 @@ def Hall(V_polar, field_range_Y, stanford_gain_1, stanford_gain_2):
     meas.register_parameter(station.mag.y_measured)
     meas.register_parameter(station.dmm1.volt, setpoints = (station.mag.y_target,))
     meas.register_parameter(station.dmm2.volt, setpoints = (station.mag.y_target,))
+    meas.register_parameter(station.dmm3.volt, setpoints = (station.mag.y_target,))
     meas.register_custom_parameter("R_h", unit = "Ohms", setpoints = (station.mag.y_target,))
     meas.register_custom_parameter("R_xx", unit = "Ohms", setpoints = (station.mag.y_target,))
 
@@ -54,33 +56,135 @@ def Hall(V_polar, field_range_Y, stanford_gain_1, stanford_gain_2):
             
             time.sleep(1)
             
-            volt_m = station.dmm1.volt()/stanford_gain_1
+            volt_h_m = station.dmm1.volt()/stanford_gain_1
             curr_m = station.dmm2.volt()/(R_I*stanford_gain_2)
+            volt_m = station.dmm3.volt()/stanford_gain_3
             
             time.sleep(1)
             
-            yoko.voltage(V_polar)
+            station.yoko.voltage(V_polar)
             
             time.sleep(1)
             
-            volt_p = station.dmm1.volt()/stanford_gain_1
+            volt_h_p = station.dmm1.volt()/stanford_gain_1
             curr_p = station.dmm2.volt()/(R_I*stanford_gain_2)
+            volt_p = station.dmm3.volt()/stanford_gain_3
             
             time.sleep(1)
             
             V_av = (volt_p - volt_m)/2
             I_av = (curr_p - curr_m)/2
+            V_h_av = (volt_h_p - volt_h_m)/2
             
             R_av = V_av/I_av
-            
+            R_h_av = V_h_av/I_av
+
             print(R_av)
             
             datasaver.add_result((station.yoko.voltage, station.yoko.voltage()),
                                  (station.dmm2.volt, station.dmm2.volt()),
                                  (station.dmm1.volt, station.dmm1.volt()),
+                                 (station.dmm3.volt, station.dmm3.volt()),
                                  (station.mag.y_measured, l_y),
                                  (station.mag.y_target, b),
-                                 ("R_h", R_av))
+                                 ("R_h", R_h_av),
+                                 ("R_xx",R_av))
             station.yoko.voltage(0)
             
-    print('Done')    
+    print('Done')  
+
+
+def Hall_gate(station, v_gates, V_polar, field_range_Y, stanford_gain_1, stanford_gain_2, stanford_gain_3):
+
+    R_polar = 1e6
+
+    print(f'I_polar = {V_polar/R_polar}')
+
+    R_I = 1e4
+
+    # dmm1 is voltage
+    station.dmm1.NPLC(100)
+
+    # dmm2 is current
+    station.dmm2.NPLC(10)
+
+    station.dmm3.NPLC(100)
+    
+    station.yoko.voltage.step = 1e-4
+    station.yoko.voltage.inter_delay = 0.0001
+
+    meas = Measurement()
+
+    meas.register_parameter(station.yoko.voltage)
+    meas.register_parameter(station.mag.y_target)
+    meas.register_parameter(station.mag.y_measured)
+    meas.register_parameter(station.mdac_8.ch01.voltage)
+    meas.register_parameter(station.dmm1.volt, setpoints = (station.mag.y_target,station.mdac_8.ch01.voltage))
+    meas.register_parameter(station.dmm2.volt, setpoints = (station.mag.y_target,station.mdac_8.ch01.voltage))
+    meas.register_parameter(station.dmm3.volt, setpoints = (station.mag.y_target,station.mdac_8.ch01.voltage))
+    meas.register_custom_parameter("R_h", unit = "Ohms", setpoints = (station.mag.y_target,station.mdac_8.ch01.voltage))
+    meas.register_custom_parameter("R_xx", unit = "Ohms", setpoints = (station.mag.y_target,station.mdac_8.ch01.voltage))
+
+    with meas.run() as datasaver:
+
+        for v_g in v_gates:
+
+            station.mdac_8.ch01.ramp(v_g, 0.01)
+            station.mdac_8.ch01.block()
+            print(f'V_g = {v_g} V')
+           
+            for b in field_range_Y:
+
+                station.mag.y_target(b)
+                station.mag.ramp('simul')
+
+                while abs(station.mag.y_measured()-b)>0.001:
+                    time.sleep(2)
+                time.sleep(5)
+
+                l_y = station.mag.y_measured()
+
+                print(l_y)
+
+                station.yoko.voltage(-V_polar)
+
+                time.sleep(1)
+
+                volt_h_m = station.dmm1.volt()/stanford_gain_1
+                curr_m = station.dmm2.volt()/(R_I*stanford_gain_2)
+                volt_m = station.dmm3.volt()/stanford_gain_3
+                
+                time.sleep(1)
+                
+                station.yoko.voltage(V_polar)
+                
+                time.sleep(1)
+                
+                volt_h_p = station.dmm1.volt()/stanford_gain_1
+                curr_p = station.dmm2.volt()/(R_I*stanford_gain_2)
+                volt_p = station.dmm3.volt()/stanford_gain_3
+                
+                time.sleep(1)
+                
+                V_av = (volt_p - volt_m)/2
+                I_av = (curr_p - curr_m)/2
+                V_h_av = (volt_h_p - volt_h_m)/2
+                
+                R_av = V_av/I_av
+                R_h_av = V_h_av/I_av
+
+                print(R_av)
+                
+                datasaver.add_result((station.mdac_8.ch01.voltage, v_g),
+                                     (station.yoko.voltage, station.yoko.voltage()),
+                                     (station.dmm2.volt, station.dmm2.volt()),
+                                     (station.dmm1.volt, station.dmm1.volt()),
+                                     (station.dmm3.volt, station.dmm3.volt()),
+                                     (station.mag.y_measured, l_y),
+                                     (station.mag.y_target, b),
+                                     ("R_h", R_h_av),
+                                     ("R_xx",R_av))
+                station.yoko.voltage(0)
+                ID_exp = datasaver.run_id
+                
+        plot_by_id(ID_exp)
