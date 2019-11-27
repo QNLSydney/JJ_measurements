273b3241a581348d6375ed6118a1d7dd0b4a6bad
Alexis Jouan <alexis.jouan@sydney.edu.au>
2019-11-27 11:50:17 +1100

    GV Vbias with yoko

--- JJ_measurements/GV_Vbias.py
+++ JJ_measurements/GV_Vbias.py
@@ -234,6 +234,132 @@ def GV_IV_up(station, voltages, amplitude, stanford_gain_V_ac, stanford_gain_I, 
     station.lockin_2.sine_outdc(0)
     plot_by_id(ID_exp)
 
+def GV_IV_yoko_up(station, voltages, amplitude, stanford_gain_V_ac, stanford_gain_I, stanford_gain_V):
+
+
+    now = datetime.now()
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string)    
+
+    print(f'Stanford Gain V_AC ={stanford_gain_V_ac}')
+    print(f'Stanford Gain I_DC ={stanford_gain_I}')
+    print(f'Stanford Gain V_DC ={stanford_gain_V}')
+    print(f'V_max = {voltages[-1]} V')
+
+    int_time = 1 #Integration time of the dmm's
+
+    station.dmm1.volt()
+    station.dmm1.NPLC(int_time)
+
+    station.dmm2.volt() 
+    station.dmm2.NPLC(int_time)
+
+    #station.dmm2.volt() 
+    #station.dmm2.NPLC(int_time)
+
+    print(f'Integration time DC = {int_time*0.02} s')
+
+    time_constant = station.lockin_2.time_constant()
+
+    print(f'Integration time lockins {time_constant} s')
+
+    #print(f'Stanford Gain V ={stanford_gain_V}') TO DO
+    #print(f'Stanford Gain I ={stanford_gain_I}')
+    print(f'Voltage Max V_max = {voltages[-1]}')
+
+    #station.yoko.output('off')
+    #station.yoko.source_mode("VOLT")
+    #station.yoko.output('on')
+
+    station.yoko.voltage.step = 0.1e-6
+    station.yoko.voltage.inter_delay = 5e-4
+    #
+    station.lockin_2.amplitude(amplitude)
+
+    meas = Measurement()
+
+    
+    meas.register_parameter(station.yoko.voltage)
+    meas.register_parameter(station.lockin_2.amplitude)
+    meas.register_parameter(station.dmm1.volt)
+    meas.register_parameter(station.dmm2.volt, setpoints=(station.dmm1.volt,))
+    meas.register_parameter(station.lockin_1.Y, setpoints=(station.dmm1.volt,))
+    meas.register_parameter(station.lockin_2.Y, setpoints=(station.dmm1.volt,))
+    meas.register_parameter(station.lockin_1.X, setpoints=(station.dmm1.volt,))
+    meas.register_parameter(station.lockin_2.X, setpoints=(station.dmm1.volt,))
+    meas.register_custom_parameter("G_ac", unit="S", setpoints=(station.dmm1.volt,))
+    meas.register_custom_parameter("I_dc", unit="A", setpoints=(station.dmm1.volt,))
+
+    print(f'Frequency Lockin : {station.lockin_1.frequency()} Hz')
+
+    station.lockin_2.amplitude(amplitude)
+    print(f'V_ac polarization : {amplitude*1e3} mV')
+
+    print(f'Filter lockin 1 : {station.lockin_1.filter_slope()} dB roll off')
+    print(f'Sensitivity lockin 1 : {station.lockin_1.sensitivity()} V')
+
+    print(f'Filter lockin 2 : {station.lockin_2.filter_slope()} dB roll off')
+    print(f'Sensitivity lockin 2 : {station.lockin_2.sensitivity()} A')
+   
+
+    v_init = voltages[0]
+    v_final = voltages[-1]
+    L = int(len(voltages)/2)
+    volt_sweep_init = np.linspace(0.0, v_init, L)
+    volt_sweep_final = np.linspace(v_final, 0.0, L)
+    
+
+    with meas.run() as datasaver:
+
+        for v in volt_sweep_init:
+
+            station.yoko.voltage(v)
+            time.sleep(100e-3)
+
+        time.sleep(10)
+
+        for v in voltages:
+
+            station.yoko.voltage(v)
+
+            v_dc = station.dmm1.volt()/stanford_gain_V
+
+            i_dc = -station.dmm2.volt()/stanford_gain_I #Using ithaco
+
+            #i_dc = v_i_dc/R_I
+
+            time.sleep(9*time_constant)
+
+            current_X_AC = station.lockin_2.X()/stanford_gain_I
+            voltage_X_AC = station.lockin_1.X()/stanford_gain_V_ac
+
+            current_Y_AC = station.lockin_2.Y()/stanford_gain_I
+            voltage_Y_AC = station.lockin_1.Y()/stanford_gain_V_ac
+
+            G_ac = current_X_AC/voltage_X_AC
+
+            datasaver.add_result(("G_ac",G_ac),
+                                (station.dmm1.volt, v_dc),
+                                (station.dmm2.volt, v_i_dc),
+                                ("I_dc",i_dc),
+                                (station.yoko.voltage, v),
+                                (station.lockin_1.Y,current_Y_AC),
+                                (station.lockin_2.Y,voltage_Y_AC),
+                                (station.lockin_1.X,current_X_AC),
+                                (station.lockin_2.X,voltage_X_AC))
+
+        for v in volt_sweep_final:
+
+            station.yoko.voltage(v)
+            time.sleep(100e-3)
+
+
+        ID_exp = datasaver.run_id
+
+    station.yoko.voltage(0)
+    plot_by_id(ID_exp)
+
 
 def GV_2D(station, voltages, v_gates, amplitude, stanford_gain_V_ac, stanford_gain_V, stanford_gain_I):
     
